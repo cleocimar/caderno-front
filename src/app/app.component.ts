2fcdef0d5c154a8e18b6d7f36b508681e268e16b
CLEOCIMAR GOMES DA SILVA <cleocimar@users.noreply.github.com>
2024-10-16 01:51:42 -0400

    exemplo do video funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'caderno-front';
+  nome = 'mundo';
+  frutas = ['maça','uva','laranja'];
+  fruta = '';
+
+  alterarFruta() {
+    this.frutas.push(this.fruta)
+    this.fruta = 'Variavel Alterada';
+  }
 }
